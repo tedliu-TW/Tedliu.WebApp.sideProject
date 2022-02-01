85bfb3fdc39a2a2442c38ea1b9cd7ac4cb42d1b4
tedliu-TW <53557242+tedliu-TW@users.noreply.github.com>
2022-02-01 15:52:09 +0800

    add         [Required]         [StringLength(50)]         [DisplayName("Email")]         [EmailAddress(ErrorMessage ="這不是Email格式")]

--- Models/Member.cs
+++ Models/Member.cs
@@ -2,14 +2,18 @@ namespace Tedliu.WebApp.sideProject.Models
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Data.Entity.Spatial;
+    using System.Web.Mvc;
 
     public partial class Member
     {
         [Key]
         [StringLength(50)]
+        [DisplayName("帳號")]
+        [Remote("AccountCheck","Member",ErrorMessage ="此帳號已經被註冊過")]
         public string Account { get; set; }
 
         [Required]
@@ -18,10 +22,13 @@ namespace Tedliu.WebApp.sideProject.Models
 
         [Required]
         [StringLength(50)]
+        [DisplayName("姓名")]
         public string Name { get; set; }
 
         [Required]
         [StringLength(50)]
+        [DisplayName("Email")]
+        [EmailAddress(ErrorMessage ="這不是Email格式")]
         public string Email { get; set; }
 
         [Required]
